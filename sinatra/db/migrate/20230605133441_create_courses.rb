5fe5722494e2966cb7b9951281baeb5002e8ebe5
henry cheche <henry.cheche@student.moringaschool.com>
2023-06-05 20:44:50 +0300

    Defined relationships

--- sinatra/db/migrate/20230605133441_create_courses.rb
+++ sinatra/db/migrate/20230605133441_create_courses.rb
@@ -1,4 +1,12 @@
 class CreateCourses < ActiveRecord::Migration[6.1]
   def change
+    create_table :courses do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
   end
 end
+
--- sinatra/db/migrate/20230605133506_create_applications.rb
+++ sinatra/db/migrate/20230605133506_create_applications.rb
@@ -1,4 +1,14 @@
 class CreateApplications < ActiveRecord::Migration[6.1]
   def change
+    create_table :applications do |t|
+      t.string :firstname
+      t.string :secondname
+      t.string :date_of_birth
+      t.string :gender
+      t.integer :phone_number
+      t.string :email
+      t.references :course, foreign_key: true
+      t.timestamps
+    end
   end
 end
