fb7ce8e040e52a9b72c2308214992e08ee606c83
henry cheche <henry.cheche@student.moringaschool.com>
2023-06-08 13:25:57 +0300

    Deploy

--- sinatra/config/environment.rb
+++ sinatra/config/environment.rb
@@ -5,8 +5,8 @@ ENV['RACK_ENV'] ||= "development"
 # Require in Gems
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
- # #configure
-  configure :development do
+ # Database configrurations
+configure do
     set :database, {
       adapter: 'sqlite3',
       database: 'db/development.sqlite3'
