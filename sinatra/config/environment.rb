dff1cb8e3754ec5c83809444a3e7810decae6507
henry cheche <henry.cheche@student.moringaschool.com>
2023-06-08 19:46:04 +0300

    Deploy

--- sinatra/config/environment.rb
+++ sinatra/config/environment.rb
@@ -1,4 +1,4 @@
-ENV['RACK_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "production"
 
 # Require in Gems
 require 'bundler/setup'
@@ -6,12 +6,15 @@ Bundler.require(:default, ENV['RACK_ENV'])
 
 # Database configurations
 configure do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://school:XyrQjqva9ZAueFh3nQWdMGZBmTieBJiv@dpg-ci0usj64dad5j745bjog-a.oregon-postgres.render.com/school_eaeh')
+  
   set :database, {
     adapter: 'postgresql',
-    host: 'localhost',
-    username: 'school',
-    password: 'school',
-    database: 'school'
+    host: db.host,
+    port: db.port,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1]
   }
 end
 
